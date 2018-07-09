a4cd52d771df88f4e7c809dda039589cd5359cb3
Palash <palashw173@gmail.com>
2018-07-09 23:13:02 +0530

    english to braille done

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+function convert() {
+  const inputstr = document.getElementById('sourceLangText').value;
+  const splitstr = inputstr.split('');
+  let braillestr = [];
+  for (let i = 0; i < splitstr.length; i += 1) {
+    for (let j = 0; j < englishToBrailleLiteralSet.length; j += 1) {
+      if (splitstr[i] === englishToBrailleLiteralSet[j][0]) {
+        braillestr += englishToBrailleLiteralSet[j][1];
+      }
+    }
+  }
+  document.getElementById('targetLangText').innerHTML = braillestr;
+}
+window.onload = function () {
+  document.getElementById('btnConvertEnglishToBraille').addEventListener('click', convert);
+};
